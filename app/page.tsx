47e2673d19bf8854fafad4e80c5d388d0949c61c
markxiong0122 <markxiong0122@gmail.com>
2026-02-10 12:11:42 -0600

    delete old components

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,5 @@
-import Header from "@/components/Header";
-import HeroSection from "@/components/HeroSection";
-import BusinessDescription from "@/components/BusinessDescription";
-import FeaturesSection from "@/components/FeaturesSection";
-import ProductDetails from "@/components/ProductDetails";
-import DataVisualization from "@/components/DataVisualization";
-import StatusSection from "@/components/StatusSection";
-import TeamSection from "@/components/TeamSection";
-import WaitlistForm from "@/components/WaitlistForm";
-import Footer from "@/components/Footer";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="min-h-screen">
-      <Header />
-      <HeroSection />
-      <BusinessDescription />
-      <FeaturesSection />
-      <ProductDetails />
-      <DataVisualization />
-      <StatusSection />
-      <TeamSection />
-      <WaitlistForm />
-      <Footer />
-    </main>
-  );
+  redirect("/landing/index.html");
 }
